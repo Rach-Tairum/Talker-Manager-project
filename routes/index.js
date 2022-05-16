7951e45304c83150d5e979cbac4c389023365971
Rachel Tairum <rachel.tairum@gmail.com>
2022-05-16 15:37:38 -0300

    dev: requisito 2 - identifica rotas especificas

--- routes/index.js
+++ routes/index.js
@@ -2,11 +2,19 @@ const express = require('express');
 const fs = require('fs');
 
 const routes = express.Router();
+const file = fs.readFileSync('talker.json');
+const talkers = JSON.parse(file);
 
 routes.get('/', (_req, res) => {
-  const talkers = fs.readFileSync('talker.json');
-  const talk = JSON.parse(talkers);
-  res.status(200).json(talk);
+  res.status(200).json(talkers);
+});
+
+routes.get('/:id', (req, res) => {
+  const { id } = req.params;
+  const result = talkers.find((talker) => talker.id === Number(id));
+  if (result) return res.status(200).json(result);
+
+  res.status(404).json({ message: 'Pessoa palestrante não encontrada' });
 });
 
 module.exports = routes;
