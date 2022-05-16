3e73c72d23bd99004d7dc9af80f33ff3880eedd9
Rachel Tairum <rachel.tairum@gmail.com>
2022-05-16 15:44:56 -0300

    fix: req 1

--- routes/index.js
+++ routes/index.js
@@ -2,15 +2,18 @@ const express = require('express');
 const fs = require('fs');
 
 const routes = express.Router();
-const file = fs.readFileSync('talker.json');
-const talkers = JSON.parse(file);
 
 routes.get('/', (_req, res) => {
+  const file = fs.readFileSync('talker.json');
+  const talkers = JSON.parse(file);
   res.status(200).json(talkers);
 });
 
 routes.get('/:id', (req, res) => {
   const { id } = req.params;
+  const file = fs.readFileSync('talker.json');
+  const talkers = JSON.parse(file);
+
   const result = talkers.find((talker) => talker.id === Number(id));
   if (result) return res.status(200).json(result);
 
