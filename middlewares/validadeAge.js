5f495fe2163ca1b7fe5b063122391fd9af7ade74
Rachel Tairum <rachel.tairum@gmail.com>
2022-05-18 17:21:56 -0300

    comment: validateAge e validateName

--- middlewares/validadeAge.js
+++ middlewares/validadeAge.js
@@ -1,15 +1,15 @@
 const validadeAge = (req, _res, next) => {
-  const { age } = req.body;
+  const { age } = req.body; // busca no corpo da requisição a idade passada
 
-  if (!age || age === '' || Number.isInteger(age) === false) {
-    const errorObj = { status: 400, message: 'O campo "age" é obrigatório' };
+  if (!age || age === '' || Number.isInteger(age) === false) { // verifica se a idade foi passada ou se é um número inteiro
+    const errorObj = { status: 400, message: 'O campo "age" é obrigatório' }; // objeto de erro capturado pelo error handler
     throw errorObj;
   }
 
-  if (Number(age) >= 18) {
-    return next();
+  if (Number(age) >= 18) { // verifica se o palestrante é maior de idade
+    return next(); 
   }
-  const errorObj = { status: 400, message: 'A pessoa palestrante deve ser maior de idade' };
+  const errorObj = { status: 400, message: 'A pessoa palestrante deve ser maior de idade' }; // objeto de erro capturado pelo error handler
   throw errorObj;
 };
 
--- middlewares/validadeName.js
+++ middlewares/validadeName.js
@@ -1,15 +1,15 @@
 const validadeName = (req, _res, next) => {
-  const { name } = req.body;
+  const { name } = req.body; // busca no corpo da requisição o nome passado;
 
-  if (!name || name === '') {
-    const errorObj = { status: 400, message: 'O campo "name" é obrigatório' };
+  if (!name || name === '') { // verifica se o nome foi passado
+    const errorObj = { status: 400, message: 'O campo "name" é obrigatório' }; // objeto de erro capturado pelo error handler
     throw errorObj;
   }
 
-  if (name.length > 0 && name.length >= 3) {
+  if (name.length > 0 && name.length >= 3) { // verifica se o nome tem no mínimo 3 letras
     return next();
   }
-  const errorObj = { status: 400, message: 'O "name" deve ter pelo menos 3 caracteres' };
+  const errorObj = { status: 400, message: 'O "name" deve ter pelo menos 3 caracteres' }; // objeto de erro capturado pelo error handler
   throw errorObj;
 };
 
