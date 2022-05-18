fe44b7af3752b5758b51f4fe97a698e767bb6fa8
Rachel Tairum <rachel.tairum@gmail.com>
2022-05-18 17:08:26 -0300

    comment: errorHandler.js

--- middlewares/errorHandler.js
+++ middlewares/errorHandler.js
@@ -1,3 +1,7 @@
+// desenvolve middleware de erro, precisa que tenha os 4 parâmetros ou o 
+// express entende ele como um middlaware comum e quebra a aplicação
+// error.status - pega os status dos erros que estão sendo "jogados"
+// error.message - pega a mensagem de erro que está sendo "jogada".
 const errorHandler = (error, _req, res, _next) => {
   res.status(error.status).json({ message: error.message });
 };
